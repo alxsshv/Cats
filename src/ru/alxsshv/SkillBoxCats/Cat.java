2af69537eef70b2c00b12e20a36d9046f6b6c290
alxsshv <alxsshv@gmail.com>
2022-03-29 22:41:05 +0300

    v 3.8

--- src/ru/alxsshv/SkillBoxCats/Cat.java
+++ src/ru/alxsshv/SkillBoxCats/Cat.java
@@ -3,19 +3,54 @@ package ru.alxsshv.SkillBoxCats;
 public class Cat {
     public static int count=0;
     private double weight;
+    private String name;
 
+    // конструктор
     public Cat() {
 
         weight = 1500 + 3000 * Math.random();
         count++;
     }
 
+    // бизнес методы	
+    
     public static int getCount(){
         return count;
     }
+    
+    //методы изменения веса кошки
     public void setWeight(double catWeight) {
-        weight = catWeight;
+    
+    	weight = catWeight;
+    
     }
+    
+    public double getWeight () {
+    
+    	return weight;
+    
+    }
+    
+    // методы изменения имени кошки
+    public String getName () {
+    
+    return	name; 
+    	
+    }
+    
+    public void setName (String catName) {
+    	name = catName;
+    }
+    
+    //метод копирования кошки
+    public Cat catCoping () {
+    	Cat e = new Cat();
+    	e.name = this.name;
+    	e.weight = this.weight;
+    	return e;
+    }
+    
+    // состояние кошки
     public String getStatus(){
         if (weight < 1000) {
            count--;
--- src/ru/alxsshv/SkillBoxCats/Loader.java
+++ src/ru/alxsshv/SkillBoxCats/Loader.java
@@ -3,15 +3,22 @@ package ru.alxsshv.SkillBoxCats;
 public class Loader {
     public static void main (String [] args){
 
-        Cat vasia = new Cat();
+        Cat vaska;
+    	Cat vasia = new Cat();
         Cat murka = new Cat();
         Cat liska = new Cat();
 
         murka.setWeight(10000);
-
+        murka.setName("Мурка");
+       
+        vaska = murka.catCoping();
+        vaska.setName("Васька");
+        
         vasia.setWeight(5000);
 
-        System.out.println(murka.getStatus());
+        System.out.println("Имя " + murka.getName() + " вес " + murka.getWeight());
+        
+        System.out.println("Имя " + vaska.getName() + " вес " + vaska.getWeight());
 
         System.out.println("Всего создано кошек " + Cat.getCount());
 
